edf3721884356a1eca7db4529a5a73a5c9fb4edd
Joris de Vries <jorisdvries@gmail.com>
2019-02-12 05:16:34 -0600

    show_remaining_jobs.sh is now resilient to failed project.py status output.

--- show_remaining_jobs.sh
+++ show_remaining_jobs.sh
@@ -6,13 +6,32 @@ jobStatusArray=($(awk -F: '{$1=$1} 1' <<<"${jobStatus}"))
 jobsIdle=${jobStatusArray[21]}
 jobsRunning=${jobStatusArray[23]}
 
-while [ $jobsIdle != 0 ] || [ $jobsRunning != 0 ] 
+#sometimes the job status command fails and this regex checks whether $jobsIdle and $jobsRunning are numbers
+isNumber='^[0-9]+$'
+
+previousIdle=$jobsIdle
+previousRunning=$jobsRunning
+
+while [ "$jobsIdle" != 0 ] || [ "$jobsRunning" != 0 ] 
 do
     jobStatus=$(project.py --xml $xml_name --status)
     jobStatusArray=($(awk -F: '{$1=$1} 1' <<<"${jobStatus}"))
 
     jobsIdle=${jobStatusArray[21]}
     jobsRunning=${jobStatusArray[23]}
+
+    #check if number otherwise revert, wait and try again
+    if ! [[ $jobsIdle =~ $isNumber && $jobsRunning =~ $isNumber ]] ;
+    then
+        jobsIdle=$previousIdle
+        jobsRunning=$previousRunning
+
+        sleep 5
+        continue
+    fi
+
+    previousIdle=$jobsIdle
+    previousRunning=$jobsRunning
 
     echo -ne "\r\033[K > Jobs remaining: $jobsIdle idle and $jobsRunning running"
     sleep 5
